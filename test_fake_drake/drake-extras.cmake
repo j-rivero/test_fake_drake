e850fa66d2e5bd4febbf93281902d39d43767072
Jose Luis Rivero <jrivero@osrfoundation.org>
2024-01-19 15:40:10 +0100

    Update drake-extras.cmake
    Clean up debug code

--- test_fake_drake/drake-extras.cmake
+++ test_fake_drake/drake-extras.cmake
@@ -1,5 +1,3 @@
 list(APPEND CMAKE_PREFIX_PATH "opt/drake")
-list(APPEND AMENT_PREFIX_PATH "opt/drake")
 
-message(STATUS "FINDING DRAKE IN EXTRAS: ${CMAKE_PREFIX_PATH}")
 find_package(drake PATHS /opt/drake)
